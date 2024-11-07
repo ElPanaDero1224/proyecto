95e9f55798ba86f303d1091b8cd9965b4a203cce
SamuelVeytia <samveytia6@gmail.com>
2024-11-07 01:28:43 -0600

    Commit 1
    Incluye vistas de /notificacionesProgramados
    /reservasAdmi

--- proyectoPW/routes/veytia.php
+++ proyectoPW/routes/veytia.php
@@ -1,9 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\controladorreservasAdmi;
+use App\Http\Controllers\controladornotificacionesProgramados;
 
 // Route::get('/prueba', function () {
 //     return view('welcome');
 // });
 
-Route::view('/prueba','welcome');
+Route::get('/reservasAdmi',[controladorreservasAdmi::class, 'controladorreservasAdmi'])->name('rutareservasAdmi');
+Route::get('/notificacionesProgramados',[controladornotificacionesProgramados::class, 'controladornotificacionesProgramados']);
+
+Route::post('/reservasAdmiexportar', [controladorreservasAdmi::class, 'controladorreservasAdmialert']);
